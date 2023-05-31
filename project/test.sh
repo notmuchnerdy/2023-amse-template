a9445e32dbcbc5e14f9bf4d6e74dcb5f08c025a3
notmuchnerdy <osmanyigit_95@hotmail.com>
2023-05-31 09:38:14 +0200

    added test.sh file

--- project/test.sh
+++ project/test.sh
@@ -1,22 +1,5 @@
-import os
+#!/bin/bash
 
-print(os.path.abspath(os.curdir))
-os.chdir("..")
-with open(".\data\data_pull.py") as f:
-    exec(f.read())
-    f.close()
 
-sqlite_files = [f for f in os.listdir('.') if f.endswith('.sqlite')]
-if len(sqlite_files)>=1:
-    name_of_the_file=sqlite_files[0]
-
-try:
-    file=open(".\data\{}".format(name_of_the_file))
-    print("Test passed, {} file could be found successfully!".format(name_of_the_file))
-    
-
-except IOError:
-    print("Test failed, {} file could not be found!".format(name_of_the_file))
-
-finally:
-    file.close()
+# Run the Python script and check if it generates the SQLite file
+python "test.py"
