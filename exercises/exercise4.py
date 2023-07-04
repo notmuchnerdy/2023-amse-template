78559bedfff329ecc54f50520bb662c95f0b72f7
notmuchnerdy <osmanyigit_95@hotmail.com>
2023-07-04 11:50:10 +0200

    Exercise4.py added

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -16,23 +16,26 @@ with ZipFile('{}.zip'.format(file_name), 'r') as f:
     f.extractall()
 
 #Transform
-selected_columns= ["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"]
-
-df = pd.read_csv("data.csv",delimiter=";",usecols=range(11),decimal=",",header=None)
+num_cols=11
+df = pd.read_csv("data.csv",delimiter=";",usecols=range(num_cols),decimal=",",header=None)
 header=list(df.iloc[0])
 df=df[1:]
 df.columns=header
 df=df.applymap(lambda x: str(x.replace(',','.')))
 
+#Selection of only intended columns
 selected_columns= ["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"]
 df=df[selected_columns]
 
+#Changing the data types of the columns
 df[["Hersteller","Geraet aktiv","Model"]] = df[["Hersteller","Geraet aktiv","Model"]].astype(str)
 df[["Geraet", "Monat"]] = df[["Geraet", "Monat"]].astype(int)
 df[["Temperatur in °C (DWD)", "Batterietemperatur in °C"]] = df[["Temperatur in °C (DWD)", "Batterietemperatur in °C"]].astype(float)
 
+#Renaming the columns
 df=df.rename(columns={"Temperatur in °C (DWD)": "Temperatur", "Batterietemperatur in °C": "Batterietemperatur"})
 
+#Temperature Conversion
 df["Temperatur"]=(df["Temperatur"]*9/5)+32
 
 df["Batterietemperatur"]=(df["Batterietemperatur"]*9/5)+32
